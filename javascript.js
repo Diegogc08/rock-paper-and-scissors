01b495b62d676f28298ecb715fdc1557cfda879d
Diegogc29 <diego30stm@hotmail.com>
2025-05-17 21:25:28 -0600

    terminado

--- javascript.js
+++ javascript.js
@@ -1,25 +1,29 @@
 //elección de jugador//
-let humanChoice = parseInt(prompt("selecciona 1 para piedra, 2 para papel y 3 para tijera" ));
+let HumanScore = 0;
+let computerScore = 0; 
 
-function getHumanChoice () { 
-  return (humanChoice);
-}
-if (humanChoice == 1) {
-  console.log("Ha elegido Piedra");
-}
-if (humanChoice == 2) {
-  console.log("Ha elegido Papel")
-}
-if (humanChoice == 3) {
-  console.log("Ha elegido Tijera")
-}
-//______________________________________//
+while (HumanScore < 3 && computerScore < 3) {
+  let humanChoice = parseInt(prompt("selecciona 1 para piedra, 2 para papel y 3 para tijera"));
+  function getHumanChoice() {
 
-//elección de computadora//
-function getComputerChoice() {
+    return (humanChoice);
+  }
+  if (humanChoice == 1) {
+    console.log("Ha elegido Piedra");
+  }
+  if (humanChoice == 2) {
+    console.log("Ha elegido Papel")
+  }
+  if (humanChoice == 3) {
+    console.log("Ha elegido Tijera")
+  }
+  //______________________________________//
+
+  //elección de computadora//
+  function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1;
   }
-  
+
   let computerChoice = getComputerChoice();
   if (computerChoice == 1) {
     console.log("Comp eligió Piedra");
@@ -27,35 +31,29 @@ function getComputerChoice() {
   if (computerChoice == 2) {
     console.log("Comp eligió Papel");
   }
+
   if (computerChoice == 3) {
     console.log("Comp eligió Tijera");
   }
-//__________________________________________//
-let HumanScore = 0;
-let computerScore = 0; 
+  //__________________________________________//
 
-  function playRound (HumanScore, computerScore) {
-    return console.log("Tu puntuación es " + HumanScore  + " y la computadora " + computerScore);
+  function playRound(HumanScore, computerScore) {
+    return console.log("Tu puntuación es " + HumanScore + " y la computadora " + computerScore);
   }
-  
-  
 
   if (humanChoice === 1 && computerChoice === 3) {
-    HumanScore = HumanScore +1;
+    HumanScore = HumanScore + 1;
     console.log("Ganaste");
-    console.log(HumanScore);
   }
   else if (humanChoice === 2 && computerChoice === 1) {
-    HumanScore = HumanScore +1;
+    HumanScore = HumanScore + 1;
     console.log("Ganaste");
   }
   else if (humanChoice === 3 && computerChoice === 2) {
-    HumanScore = HumanScore +1;
+    HumanScore = HumanScore + 1;
     console.log("ganaste");
   }
   else if (humanChoice === computerChoice) {
-    HumanScore = HumanScore;
-    computerScore = computerScore;
     console.log("Empate");
   }
   else {
@@ -63,5 +61,10 @@ let computerScore = 0;
     console.log("Perdiste");
   }
   playRound (HumanScore, computerScore);
-  
-  //bucle//
+}
+
+if (HumanScore === 3) {
+  alert("¡Ganaste el juego!");
+} else if (computerScore === 3) {
+  alert("¡Perdiste el juego!");
+}
